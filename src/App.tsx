975d805ac9635d6742100aa1980d58583d33d757
epons <ticquique@gmail.com>
2021-10-21 08:47:51 +0200

    added prettier, eslint config and formated files

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return <div className="App">hello</div>;
+  return <div className="App">hello</div>;
 }
 
 export default App;
